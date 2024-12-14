15a5f406d6b03b6275f9435c4af354247eb20130
yang <3075074447@qq.com>
2024-12-14 17:47:40 +0800

    login register

--- blog-java/src/main/java/fun/aprilsxz/blog/advice/CommonExceptionAdvice.java
+++ blog-java/src/main/java/fun/aprilsxz/blog/advice/CommonExceptionAdvice.java
@@ -1,12 +1,18 @@
 package fun.aprilsxz.blog.advice;
 
 import fun.aprilsxz.blog.domain.common.Result;
+import fun.aprilsxz.blog.exception.BadRequestException;
 import fun.aprilsxz.blog.exception.CommonException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.util.WebUtils;
+
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -19,29 +25,29 @@ public class CommonExceptionAdvice {
 //        return processResponse(e);
 //    }
 
-//    @ExceptionHandler(CommonException.class)
-//    public Object handleBadRequestException(CommonException e) {
-//        log.error("自定义异常 -> {} , 异常原因：{}  ",e.getClass().getName(), e.getMessage());
-//        log.debug("", e);
-//        return processResponse(e);
-//    }
+    @ExceptionHandler(CommonException.class)
+    public Object handleBadRequestException(CommonException e) {
+        log.error("自定义异常 -> {} , 异常原因：{}  ",e.getClass().getName(), e.getMessage());
+        log.debug("", e);
+        return processResponse(e);
+    }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        String msg = e.getBindingResult().getAllErrors()
-//                .stream().map(ObjectError::getDefaultMessage)
-//                .collect(Collectors.joining("|"));
-//        log.error("请求参数校验异常 -> {}", msg);
-//        log.debug("", e);
-//        return processResponse(new BadRequestException(msg));
-//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getAllErrors()
+                .stream().map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining("|"));
+        log.error("请求参数校验异常 -> {}", msg);
+        log.debug("", e);
+        return processResponse(new BadRequestException(msg));
+    }
 
-//    @ExceptionHandler(BindException.class)
-//    public Object handleBindException(BindException e) {
-//        log.error("请求参数绑定异常 ->BindException， {}", e.getMessage());
-//        log.debug("", e);
-//        return processResponse(new BadRequestException("请求参数格式错误"));
-//    }
+    @ExceptionHandler(BindException.class)
+    public Object handleBindException(BindException e) {
+        log.error("请求参数绑定异常 ->BindException， {}", e.getMessage());
+        log.debug("", e);
+        return processResponse(new BadRequestException("请求参数格式错误"));
+    }
 
 //    @ExceptionHandler(NestedServletException.class)
 //    public Object handleNestedServletException(NestedServletException e) {
